c1d655885342b753a41aa1eaa5cf15b31bd850a0
Partho Roy <roypartho772@gmail.com>
2023-05-21 18:01:23 +0600

    vercel deployed

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: "",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.SERVER_SECRET_USER}:${process.env.SERVER_SECRET_PASS}@cluster0.h0arnkr.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,7 +29,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
     const toysCollection = client.db("toysDB").collection("toysCollection");
 
