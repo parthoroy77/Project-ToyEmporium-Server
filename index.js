4f3ac12515a0428162203cc1d4a85d58adde198b
Partho Roy <roypartho772@gmail.com>
2023-05-21 14:26:34 +0600

    single data filtered by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,6 +37,13 @@ async function run() {
 
       app.get('/subCategory/:category', async (req, res) => {
           const result = await toysCollection.find({ subCategory: req.params.category }).toArray();
+          res.send(result)
+      })
+
+      app.get('/details/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) };
+          const result = await toysCollection.findOne(filter);
           res.send(result)
       })
 
