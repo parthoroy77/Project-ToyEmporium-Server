222553d3521008cc9f426fc8897d6ed22e86129c
Partho Roy <roypartho772@gmail.com>
2023-05-21 19:02:42 +0600

    data updated function added

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-const corsConfig = {
-  origin: "",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.SERVER_SECRET_USER}:${process.env.SERVER_SECRET_PASS}@cluster0.h0arnkr.mongodb.net/?retryWrites=true&w=majority`;
@@ -50,26 +44,40 @@ async function run() {
       const result = await toysCollection.findOne(filter);
       res.send(result);
     });
-    app.get('/myToys', async (req, res) => {
+    app.get("/myToys", async (req, res) => {
       let query = {};
       if (req.query.email) {
-        query = {sellerEmail: req.query.email}
+        query = { sellerEmail: req.query.email };
       }
-      const result = await toysCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/addToys', async (req, res) => {
+    app.post("/addToys", async (req, res) => {
       const addToys = req.body;
       const result = await toysCollection.insertOne(addToys);
-      res.send(result)
-    })
-    app.delete('/deleteToy/:id', async (req, res) => {
+      res.send(result);
+    });
+    app.put("/updateData/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          price: updatedData.price,
+          quantity: updatedData.quantity,
+          description: updatedData.description,
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.delete("/deleteToy/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await toysCollection.deleteOne(filter);
-      res.send(result)
-    })
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
